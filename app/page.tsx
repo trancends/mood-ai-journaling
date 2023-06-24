e75f10726d8d0615dd79a00840fbf5dd8c64e0b4
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-06-24 22:15:20 +0700

    add sign in and sign out using clerk, add prisma for orm, and add database at planetscale

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
-  return <div>Home Page</div>;
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? "/journal" : "/new-user";
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-white  ">
+      <div className="mx-auto w-full max-w-[600px]">
+        <h1 className="mb-4 text-6xl">The best Journal app, period.</h1>
+        <p className="mb-4 text-2xl text-white/60">
+          This is the best app for tracking your mood through out your life. All
+          you have todo is be honest
+        </p>
+        <div>
+          <Link href={href}>
+            <button className="rounded-lg bg-violet-600 px-4 py-2 text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
