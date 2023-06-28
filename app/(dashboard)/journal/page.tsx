057478970fad8f9b4474f2b5cdc4a41b1cec2ca1
Benedictus Jullian <benedictusjullian1@gmail.com>
2023-06-28 16:21:59 +0700

    create a prompt schema using zod and then parse it using langchain so it can be use as a template

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -16,14 +16,8 @@ const getEntries = async () => {
     },
   });
 
-  await analyze(`I'm going to give you a journal entry,  I want you to analyze 
-  for a few things. I need the mood, a summary, what the subject is, and a 
-  color representing the mood. You need to respond back with formatted JSON 
-  like so: {"mood":'',"subject":'', "color":' ', "negative":' '}. 
-  
-  entry:
-  Today was a really great day. I finally was able to grab that pair of shoes I
-  have been dying to get`);
+  await analyze(`Today was a eh, ok day I guess. I found a new coffee shop that 
+  was cool but then I got a flat tire. :)`);
 
   return entries;
 };
